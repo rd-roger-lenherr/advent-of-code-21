039f56f56039e1649173ca34775001b2a4d72c53
Roger Lenherr <roger.lenherr@rewe-digital.com>
2021-12-09 13:18:16 +0100

    Day 9 part1

---
+++ day9.clj
@@ -0,0 +1,35 @@
+(ns day9
+  (:require [clojure.string :as str]))
+
+(def test-sample
+  "2199943210
+3987894921
+9856789892
+8767896789
+9899965678")
+
+(def lines->matrix
+  (partial mapv (comp (partial mapv (comp read-string str)) seq)))
+
+(defn- minima?
+  [[y x :as cself] matrix]
+  (let [self (get-in matrix cself)
+        in #(get-in matrix % 9)]
+    (< self (min (in [(dec y) x])
+                 (in [(inc y) x])
+                 (in [y (dec x)])
+                 (in [y (inc x)])))))
+
+(defn- minima [matrix]
+  (->> (for [y (range (count matrix))
+             x (range (count (first matrix)))]
+         (when (minima? [y x] matrix)
+           (get-in matrix [y x])))
+       (remove nil?)))
+
+(->> (slurp "input9.txt")
+     (str/split-lines)
+     lines->matrix
+     minima
+     (map inc)
+     (reduce +))
