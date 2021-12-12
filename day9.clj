949d62c80cd129424236d31f317567a5627bea16
Roger Lenherr <roger.lenherr@rewe-digital.com>
2021-12-12 21:51:13 +0100

    Debug day 9 part 2

--- day9.clj
+++ day9.clj
@@ -1,5 +1,6 @@
 (ns day9
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
 
 (def test-sample
   "2199943210
@@ -32,11 +33,49 @@
        (map (comp inc (partial get-in matrix)))
        (reduce +)))
 
+(defn- neighbors+ [matrix [yref xref]]
+  (for [y (range (max 0 (dec yref)) (inc (min (inc yref) (dec (count matrix)))))
+        x (range (max 0 (dec xref)) (inc (min (inc xref) (dec (count (first matrix))))))
+        :when (or (and (= y yref) (not= x xref))
+                  (and (= x xref) (not= y yref)))]
+    [y x]))
+
+(defn- risen? [matrix ref-cord cord]
+  (let [expected (inc (get-in matrix ref-cord))]
+    (when (and (< expected 9) (= expected (get-in matrix cord)))
+      cord)))
+
+(defn- basin [matrix found cord]
+  (let [check-next (some->> (neighbors+ matrix cord)
+                            (map (partial risen? matrix cord))
+                            (remove nil?))
+        found-next (conj found cord)]
+    (if (empty? check-next)
+      found-next
+      (reduce (fn [found cord] (basin matrix found cord))
+              found-next check-next))))
+
+(defn- part2 [matrix]
+  (->> matrix
+       (minima)
+       (map (comp #_count set (partial basin matrix '())))
+       ;; (sort >)
+       ;; (take 3)
+       #_(apply *)))
+
 (let [matrix (->> (or test-sample (slurp "input9.txt"))
                   str/split-lines
                   lines->matrix)]
   ;; (println "Part 1:" (part1 matrix))
-  (->> matrix
-       minima))
+  ;; (println "Part 2:" (part2 matrix))
+  (println :IS)
+  (clojure.pprint/pprint (->> (part2 matrix)
+                              (reduce into #{})
+                              (map (partial get-in matrix))
+                              (frequencies)
+                              (into (sorted-map))))
+  (clojure.pprint/pprint (dissoc (into (sorted-map) (frequencies (flatten matrix))) 9))
+  (println :SHOULD)
+  )
 
 
