584035496af9ab38eeb75511dea316eadb2ec350
Roger Lenherr <roger.lenherr@rewe-digital.com>
2021-12-12 13:29:42 +0100

    Redefine input types

--- day12.clj
+++ day12.clj
@@ -1,4 +1,5 @@
-(ns day12)
+(ns day12
+  (:require [clojure.string :as str]))
 
 (def test-sample
   "start-A
@@ -9,7 +10,16 @@ b-d
 A-end
 b-end")
 
+(defn- small? [k] (let [s (name k)] (= s (str/lower-case s))))
+(def big? (complement small?))
+
+(defn- chart [input]
+  )
+
 (let [debug? true
       input (->> (if debug? test-sample (slurp "input12.txt"))
-                 )]
-  #_eof)
+                 (str/split-lines)
+                 (map #(str/split % #"-"))
+                 (reduce (fn [m [k v]] (update m (keyword k) (fnil conj #{}) (keyword v))) {}))]
+  (->> input
+       #_eof))
