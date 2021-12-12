7e07ac22db28ec5be2b107636400ce29e8ff7319
Roger Lenherr <roger.lenherr@rewe-digital.com>
2021-12-12 15:03:57 +0100

    Save day 12 part 2 for later

--- day12.clj
+++ day12.clj
@@ -40,14 +40,14 @@ start-RW")
     (set/difference (get input current) taken)))
 
 (defn- chart
-  ([input] (chart input (list) (list) :start))
-  ([input paths path current]
-   (let [choices (options input path current)
+  ([choice-fn input] (chart choice-fn input (list) (list) :start))
+  ([choice-fn input paths path current]
+   (let [choices (choice-fn input path current)
          current-path (conj path current)]
      (if (empty? choices)
        (conj paths current-path)
        (reduce (fn [ps choice]
-                 (chart input ps current-path choice))
+                 (chart choice-fn input ps current-path choice))
                paths choices)))))
 
 (defn tuples->input
@@ -62,19 +62,74 @@ start-RW")
                 (= k :start)  (update v set/difference #{:start}))))
           {} tuples))
 
-(defn- part1
-  [input]
-  (->> (chart input)
+(defn- solve
+  [choice-fn input]
+  (->> (chart choice-fn input)
        (filter (comp #{:end} first))
+       (#(doto % (->> (map reverse) (group-by second) clojure.pprint/pprint)))
        (count)))
+
+(defn options-2
+  [input path current]
+  (let [double? (->> (frequencies path)
+                     (some (fn [[k v]] (when (#{2} v) k))))]
+    (cond-> (get input current)
+      double? (set/difference (->> path (filter small?) set)))))
 
 (let [sample test-sample
       input (->> (or sample (slurp "input12.txt"))
                  (str/split-lines)
                  (map #(str/split % #"-"))
                  (tuples->input))]
-  (println (part1 input))
-  (-> input
-      (doto (println))
-        ;; (chart)
-      #_eof))
+  ;; (println "Part 1:" (solve options input))
+  (println "Part 2:" (solve options-2 input)))
+
+
+(set/difference
+ (->>
+  "start,A,b,A,b,A,c,A,end
+start,A,b,A,b,A,end
+start,A,b,A,b,end
+start,A,b,A,c,A,b,A,end
+start,A,b,A,c,A,b,end
+start,A,b,A,c,A,c,A,end
+start,A,b,A,c,A,end
+start,A,b,A,end
+start,A,b,d,b,A,c,A,end
+start,A,b,d,b,A,end
+start,A,b,d,b,end
+start,A,b,end
+start,A,c,A,b,A,b,A,end
+start,A,c,A,b,A,b,end
+start,A,c,A,b,A,c,A,end
+start,A,c,A,b,A,end
+start,A,c,A,b,d,b,A,end
+start,A,c,A,b,d,b,end
+start,A,c,A,b,end
+start,A,c,A,c,A,b,A,end
+start,A,c,A,c,A,b,end
+start,A,c,A,c,A,end
+start,A,c,A,end
+start,A,end"
+  (str/split-lines)
+  (map #(str/split % #","))
+  (map (partial map keyword))
+  (into #{}))
+
+ (into #{}
+          '((:start :A :end)
+            (:start :A :b :end)
+            (:start :A :b :d :b :end)
+            (:start :A :b :d :b :A :end)
+            (:start :A :b :d :b :A :c :A :end)
+            (:start :A :b :A :end)
+            (:start :A :b :A :b :end)
+            (:start :A :b :A :b :A :end)
+            (:start :A :b :A :b :A :c :A :end)
+            (:start :A :b :A :c :A :end)
+            (:start :A :c :A :end)
+            (:start :A :c :A :b :end)
+            (:start :A :c :A :b :A :end)
+            (:start :A :c :A :c :A :end)
+            (:start :A :c :A :c :A :b :end)
+            (:start :A :c :A :c :A :b :A :end))))
