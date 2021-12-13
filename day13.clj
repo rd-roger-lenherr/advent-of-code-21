1ffe6da49ffb1e33d5c19cf9fa253422d0c2ac64
Roger Lenherr <roger.lenherr@rewe-digital.com>
2021-12-13 11:20:30 +0100

    Day 13 part 2

--- day13.clj
+++ day13.clj
@@ -1,29 +1,6 @@
 (ns day13
   (:require [clojure.string :as str]))
 
-(def test-sample
-  "6,10
-0,14
-9,10
-0,3
-10,4
-4,11
-6,0
-6,12
-4,1
-0,13
-10,12
-3,4
-3,0
-8,4
-1,10
-2,14
-8,10
-9,0
-
-fold along y=7
-fold along x=5")
-
 (defn- coord [s] (->> (str/split s #",") (mapv read-string)))
 (defn- fold [s]
   (let [[_ axis v] (re-find #"(x|y)=(\d+)" s)]
@@ -31,27 +8,43 @@ fold along x=5")
 
 (defn- fold-fn [[on-x on-y]]
   (fn [[x y]]
-    (if (pos? on-y) ;; HINT: impossible to fold on 0 
-      (let [max-y (-> on-y (* 2) inc)]
-        (when (and (< y max-y) (not= y on-y))
-          (if (< y on-y)
-            [x y]
-            [x (- (dec max-y) y)])))
-      (let [max-x (-> on-x (* 2) inc)]
-        (when (and (< x max-x) (not= x on-x))
-          (if (< x on-x)
-            [x y]
-            [(- (dec max-x) x) y]))))))
+    (cond
+      (nil? (or x y)) nil
+      (pos? on-y) (let [max-y (-> on-y (* 2) inc)]
+                    (when (and (< y max-y) (not= y on-y))
+                      (if (< y on-y)
+                        [x y]
+                        [x (- (dec max-y) y)])))
+      (pos? on-x) (let [max-x (-> on-x (* 2) inc)]
+                    (when (and (< x max-x) (not= x on-x))
+                      (if (< x on-x)
+                        [x y]
+                        [(- (dec max-x) x) y]))))))
 
 (defn- part1 [folds coords]
   (->> (map (fold-fn (first folds)) coords)
        (into #{})
        (count)))
 
-(let [sample (and nil test-sample)
-      [scoords _ sfolds] (->> (or sample (slurp "input13.txt"))
+(defn- display [coords]
+  (let [[xr yr]
+        (->> coords
+             ((juxt (partial map first) (partial map second)))
+             (map (comp range inc (partial apply max))))]
+    (for [y yr
+          x xr]
+      (do (when (zero? x) (println))
+          (doto (if (coords [x y]) "#" " ") print)))))
+
+(defn- part2 [folds coords]
+  (let [all-folds (->> (reverse folds) (map fold-fn) (apply comp))]
+    (->> (map all-folds coords)
+         (into #{}))))
+
+(let [[scoords _ sfolds] (->> (slurp "input13.txt")
                               (str/split-lines)
                               (partition-by empty?))
       coords (map coord scoords)
       folds (map fold sfolds)]
-  (println "Part 1:" (part1 folds coords)))
+  (println "Part 1:" (part1 folds coords))
+  (display (part2 folds coords)))
