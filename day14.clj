57836abdcfc88c60b38fc1991a9a402ad4cf373e
Roger Lenherr <roger.lenherr@rewe-digital.com>
2021-12-14 12:57:13 +0100

    Day 14 part 2 dirty

--- day14.clj
+++ day14.clj
@@ -48,6 +48,35 @@ CN -> C")
        ((juxt first last))
        (apply -)))
 
+(defn- step
+  [book m]
+  (reduce-kv (fn [m pair cnt]
+               (->>  (insert book [pair])
+                     (reduce #(assoc %1 %2 cnt) {})
+                    (merge-with + m)))
+             {} m))
+
+(defn- init [pairs]
+  (-> (frequencies pairs)
+      (assoc (-> pairs first reverse vec (assoc 0 nil)) 1)))
+
+(defn- reducepairs [freqs]
+  (->> freqs
+       (map (juxt #(-> % first second) second))
+       (reduce #(update %1 (first %2) (fnil + 0) (second %2)) {})))
+
+(defn- part2 [rulebook pairs]
+  (->> (range 40)
+       (reduce (fn [m _]
+                 (step rulebook m))
+               (init pairs))
+       (reducepairs)
+       ;; same as part 1 now
+       (vals)
+       (sort-by identity >)
+       ((juxt first last))
+       (apply -)))
+
 (let [sample               (comment test-sample)
       [[template] _ rules] (->> (or sample (slurp "input14.txt"))
                                 (str/split-lines)
@@ -56,5 +85,10 @@ CN -> C")
                               (map ->rule)
                               rules->book)
       pairs                (partition 2 1 template)]
-  (->> (part1 rulebook pairs)
+  (->> #_(reduce (fn [m p] (update m p (fnil inc 0))) {} pairs)
+       ;; (step rulebook (init pairs))
+       (part2 rulebook pairs)
+       ;; (map (juxt #(-> % first second) second))
+       ;; (reduce #(update %1 (first %2) (fnil + 0) (second %2)) {})
+       (println)
        #_eof))
